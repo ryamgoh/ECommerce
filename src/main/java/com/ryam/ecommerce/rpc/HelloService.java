c920659b28114874feda49252429d0c7ce1981e4
Ryann <ryanngohenrui@gmail.com>
2025-06-08 10:51:01 -0700

    Update

---
+++ src/main/java/com/ryam/ecommerce/rpc/HelloService.java
@@ -0,0 +1,23 @@
+package com.ryam.ecommerce.rpc;
+
+import com.ryam.ecommerce.internal.proto.Hello.HelloRequest;
+import com.ryam.ecommerce.internal.proto.Hello.HelloResponse;
+import com.ryam.ecommerce.internal.proto.HelloServiceGrpc;
+import io.grpc.stub.StreamObserver;
+import org.springframework.grpc.server.service.GrpcService;
+
+@GrpcService
+public class HelloService extends HelloServiceGrpc.HelloServiceImplBase {
+
+    @Override
+    public void sayHello(HelloRequest request, StreamObserver<HelloResponse> responseObserver) {
+        String greeting = "Hello, " + request.getName() + "!";
+
+        HelloResponse response = HelloResponse.newBuilder()
+                .setMessage(greeting)
+                .build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+}
--- src/test/java/com/ryam/ecommerce/ECommerceServiceTests.java
+++ src/test/java/com/ryam/ecommerce/ECommerceServiceTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ECommerceApplicationTests {
+class ECommerceServiceTests {
 
     @Test
     void contextLoads() {
